7e88401ef4bbc77152e6711717dfb42791fa6943
Jose de Jesus Zaragoza <halitohf@live.com>
2023-10-05 22:12:44 -0600

    MOD maps

--- src/app/mesa/layout.js
+++ src/app/mesa/layout.js
@@ -11,7 +11,7 @@ export default function Home() {
         </p>
         <div>
           <a
-            href="https://github.com/halitohf"
+            href="/"
             rel="noopener noreferrer"
           >
             Volver al {' '}
